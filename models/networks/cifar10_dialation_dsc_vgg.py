c924278edd6382bd0062d834c9e354e5f2223a3a
nagapavan525 <nagapavan.kalepu@gmail.com>
2020-09-12 21:30:02 +0530

    added Receptive fields at each layer

--- models/networks/cifar10_dialation_dsc_vgg.py
+++ models/networks/cifar10_dialation_dsc_vgg.py
@@ -10,39 +10,39 @@ class Cifar10NetVGG(nn.Module):
          
          #Conv Block1
          self.conv_blk1 = nn.Sequential(
-             nutils.Conv2d_BasicBlock(inC=3, outC=32, ksize=(3,3), padding=1, drop_val=drop_val), #RF=3x3,output size = 32x32x16
+             nutils.Conv2d_BasicBlock(inC=3, outC=32, ksize=(3,3), padding=1, drop_val=drop_val), #RF=3x3,output size = 32x32x32
              nutils.Conv2d_BasicBlock(inC=32, outC=32, ksize=(3,3), padding=1, drop_val=drop_val), #RF=5x5,output size = 32x32x32
              nutils.Conv2d_BasicBlock(inC=32, outC=32, ksize=(3,3), padding=1, drop_val=drop_val), #RF=7x7,output size = 32x32x64
              ) 
          
-         self.pool1 = nn.MaxPool2d(2,2)
+         self.pool1 = nn.MaxPool2d(2,2) #RF=8x8,output size = 16x16x32
          
          #Conv Block2
          self.conv_blk2 = nn.Sequential(
-             nutils.Conv2d_BasicBlock(inC=32, outC=64, ksize=(3,3), padding=2, dilation=2, drop_val=drop_val), #RF=16x16,output size = 14x14x32
-             nutils.Conv2d_BasicBlock(inC=64, outC=64, ksize=(3,3), padding=2, dilation=2, drop_val=drop_val) #RF=24x24,output size = 12x12x64
+             nutils.Conv2d_BasicBlock(inC=32, outC=64, ksize=(3,3), padding=2, dilation=2, drop_val=drop_val), #RF=16x16,output size = 16x16x64
+             nutils.Conv2d_BasicBlock(inC=64, outC=64, ksize=(3,3), padding=2, dilation=2, drop_val=drop_val) #RF=24x24,output size = 16x16x64
              ) 
          
-         self.pool2 = nn.MaxPool2d(2,2)
+         self.pool2 = nn.MaxPool2d(2,2) #RF=26x26,output size = 8x8x64
          
          #Conv Block3
          self.conv_blk3 = nn.Sequential(
-             nutils.Conv2d_BasicBlock(inC=64, outC=128, ksize=(3,3), padding=1, drop_val=drop_val), #RF=34x34,output size = 6x6x32
-             nutils.Conv2d_BasicBlock(inC=128, outC=128, ksize=(3,3), padding=1, drop_val=drop_val) #RF=42x42,output size = 6x6x64
+             nutils.Conv2d_BasicBlock(inC=64, outC=128, ksize=(3,3), padding=1, drop_val=drop_val), #RF=34x34,output size = 8x8x128
+             nutils.Conv2d_BasicBlock(inC=128, outC=128, ksize=(3,3), padding=1, drop_val=drop_val) #RF=42x42,output size = 8x8x128
              ) 
          
-         self.pool3 = nn.MaxPool2d(2,2)
+         self.pool3 = nn.MaxPool2d(2,2) #RF=46x46,output size = 4x4x128
          
          #Conv Block4
          self.conv_blk4 = nn.Sequential(
-             nutils.Conv2d_DepthWiseSeperable_BasicBlock(inC=128, outC=256, ksize=(3,3), padding=1, drop_val=drop_val), #RF=62x62,output size = 3x3x32
-             nutils.Conv2d_DepthWiseSeperable_BasicBlock(inC=256, outC=256, ksize=(3,3), padding=1, drop_val=drop_val) #RF=78x78,output size = 3x3x64
+             nutils.Conv2d_DepthWiseSeperable_BasicBlock(inC=128, outC=256, ksize=(3,3), padding=1, drop_val=drop_val), #RF=62x62,output size = 4x4x256
+             nutils.Conv2d_DepthWiseSeperable_BasicBlock(inC=256, outC=256, ksize=(3,3), padding=1, drop_val=drop_val) #RF=78x78,output size = 4x4x256
              ) 
          
          # Output Block
          self.output_block = nn.Sequential(
-             nn.AvgPool2d(kernel_size=4),                                                            #RF=94x94,output size = 1x1x64
-             nn.Conv2d(in_channels=256, out_channels=10, kernel_size=(1,1), padding=0,  bias=False),  #RF=94x94,output size = 1x1x10
+             nn.AvgPool2d(kernel_size=4),                                                            #RF=102x102,output size = 1x1x256
+             nn.Conv2d(in_channels=256, out_channels=10, kernel_size=(1,1), padding=0,  bias=False),  #RF=102x102,output size = 1x1x10
         )   
         
      def forward(self, x):
