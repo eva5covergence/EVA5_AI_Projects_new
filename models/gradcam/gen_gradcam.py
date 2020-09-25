fbd0d86fddd9b4daa6af76a4c8b5fda4ce79ca39
Ezhirko <45531125+Ezhirko@users.noreply.github.com>
2020-09-25 19:06:35 +0530

    Update gen_gradcam.py

--- models/gradcam/gen_gradcam.py
+++ models/gradcam/gen_gradcam.py
@@ -12,40 +12,43 @@ from torchvision.utils import make_grid, save_image
 
 logger = logger_utils.get_logger(__name__)
 
-def generate_grad_cam_grid(configs, classes, test_loader, device, model, matched=True):
+def generate_grad_cam_grid(configs, classes, test_loader, device, model, matched=True,num_img = 5):
+  num_images = 0
   for config in configs:
     config['arch'].to(device).eval()
   cams = [
       [cls.from_config(**config) for cls in (GradCAM,)]
       for config in configs
   ]
-  images = []
-  for gradcam, in cams:
-      num_images = 0
-      for data, target in test_loader:
-        data, target = data.to(device), target.to(device)
-        output = model(data)
-        pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
-        pred_num = pred.cpu().numpy()
-        labels = target.cpu().numpy()
-        index = 0
-        for label, predict in zip(labels, pred_num):
-          if matched:
-            if label == predict[0]: 
-              mask, _ = gradcam(data[index][np.newaxis, :])
-              heatmap, result = visualize_cam(mask, data[index][np.newaxis, :])
-              images.extend([data[index].cpu(), heatmap,result])
-              break
-          else:
-            if label != predict[0]: 
-              mask, _ = gradcam(data[index][np.newaxis, :])
-              heatmap, result = visualize_cam(mask, data[index][np.newaxis, :])
-              images.extend([data[index].cpu(), heatmap,result])
-              break
-        break
-  grid_image = make_grid(images, nrow=1)
-  unnorm_image_grid = UnNormalize(*basic_config.data['normalize_paras'])
-  unnorm_image_grid = unnorm_image_grid(torchvision.utils.make_grid(images))
-  plt.imshow(np.transpose(unnorm_image_grid, (1, 2, 0)))
-  plt.show()
-  logger.info(f"Prediction: {classes[predict[0]]}, Actual: {classes[label]}")
+  while num_images < num_img:
+      images = []
+      for gradcam, in cams:
+          for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            pred_num = pred.cpu().numpy()
+            labels = target.cpu().numpy()
+            index = 0
+            for label, predict in zip(labels, pred_num):
+              if matched:
+                if label == predict[0]: 
+                  mask, _ = gradcam(data[index][np.newaxis, :])
+                  heatmap, result = visualize_cam(mask, data[index][np.newaxis, :])
+                  images.extend([data[index].cpu(), heatmap,result])
+                  break
+              else:
+                if label != predict[0]: 
+                  mask, _ = gradcam(data[index][np.newaxis, :])
+                  heatmap, result = visualize_cam(mask, data[index][np.newaxis, :])
+                  images.extend([data[index].cpu(), heatmap,result])
+                  break
+            break
+      num_images += 1
+      grid_image = make_grid(images, nrow=1)
+      unnorm_image_grid = UnNormalize(*basic_config.data['normalize_paras'])
+      unnorm_image_grid = unnorm_image_grid(torchvision.utils.make_grid(images))
+      plt.figure(figsize=(10,10))
+      plt.imshow(np.transpose(unnorm_image_grid, (1, 2, 0)))
+      plt.show()
+      logger.info(f"Prediction: {classes[predict[0]]}, Actual: {classes[label]}")
