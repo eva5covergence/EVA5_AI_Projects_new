c83e4f2c3426947716bb15ad7b7fb851327befb0
Ezhirko <45531125+Ezhirko@users.noreply.github.com>
2020-10-01 21:11:31 +0530

    Update misc_utils.py

--- utils/misc_utils.py
+++ utils/misc_utils.py
@@ -1,5 +1,8 @@
 import torch
 import sys
+from types import ModuleType
+from configs import basic_config 
+
 from utils.logger_utils import get_logger
 
 def set_manual_seed(seed):
@@ -16,6 +19,11 @@ def get_device_type():
   use_cuda = torch.cuda.is_available()
   device = torch.device("cuda" if use_cuda else "cpu")
   return device
+  
+def current_config():
+    for item in dir(basic_config):
+      if (not item.startswith("_")) and (not isinstance(getattr(basic_config, item), ModuleType)):
+        print(f"{item} - {getattr(basic_config, item)}")
 
 if __name__ == "__main__":
     set_manual_seed(20)
