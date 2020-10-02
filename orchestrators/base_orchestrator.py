d9ddce199f1df4374c08ebc3518ef7fc92d460cb
nagapavan525 <nagapavan.kalepu@gmail.com>
2020-10-02 09:21:41 +0530

    Update base_orchestrator.py

--- orchestrators/base_orchestrator.py
+++ orchestrators/base_orchestrator.py
@@ -11,8 +11,8 @@ def start_training(EPOCHS, device, train_loader, test_loader, **models_dict):
     for model_type in models_dict:
         #print(f"Model: {model_type}")
         logger.info(f"\nModel: {model_type}\n")
-        train_accs, train_losses, test_acc, test_losses, best_model = build_model(EPOCHS, device, train_loader, test_loader, **models_dict[model_type])
-        results[model_type] = [train_accs, train_losses, test_acc, test_losses, best_model]
+        train_accs, train_losses, test_acc, test_losses, best_model,learning_rates = build_model(EPOCHS, device, train_loader, test_loader, **models_dict[model_type])
+        results[model_type] = [train_accs, train_losses, test_acc, test_losses, best_model, learning_rates]
         #print(results)
         logger.info(f"\nresults : {results}\n")
         time.sleep(10)
@@ -32,7 +32,5 @@ def display_classwise_accuracy(test_loader,device,model, classes):
                 label = labels[i]
                 class_correct[label] += c[i].item()
                 class_total[label] += 1
-
-
     for i in range(10):
         logger.info(f'Accuracy of {classes[i]} : {100 * class_correct[i] / class_total[i]}')
