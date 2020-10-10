d66e146e1de53d6f2fcd435e2004284b4fa1c86e
Ezhirko <45531125+Ezhirko@users.noreply.github.com>
2020-10-10 21:06:21 +0530

    Update gen_gradcam.py

--- models/gradcam/gen_gradcam.py
+++ models/gradcam/gen_gradcam.py
@@ -58,7 +58,7 @@ def generate_missclassified_gradcam_grid(configs, classes, test_loader, device, 
         config['arch'].to(device).eval()
     cams = [[cls.from_config(**config) for cls in (GradCAM,)] for config in configs]
     
-    plt.figure(figsize=(12,15))
+    plt.figure(figsize=(10,13))
     plt.suptitle('Misclassifications')
     num_images = 0
     
@@ -79,6 +79,46 @@ def generate_missclassified_gradcam_grid(configs, classes, test_loader, device, 
                     unnorm_image_result = unnorm_image(result)
                     p = plt.subplot((num_of_images/5),5,num_images)
                     p.imshow(np.transpose(unnorm_image_result,((1, 2, 0))))
+                    p.imshow(np.transpose(result,((1, 2, 0))))
+                    p.set_xticks(()); p.set_yticks(()) # remove ticks
+                    p.set_title(f"Prediction: {classes[predict[0]]},\n Actual: {classes[label]}")
+                index +=1
+                if num_images == num_of_images:
+                    break
+            if num_images == num_of_images:
+                break
+            
+def upgraded_missclassified_gradcam_grid(configs, classes, test_loader, device, model, matched=True,num_of_images = 25):
+    num_images = 0
+    for config in configs:
+        config['arch'].to(device).eval()
+    cams = [[cls.from_config(**config) for cls in (GradCAM,)] for config in configs]
+    
+    plt.figure(figsize=(10,15))
+    plt.suptitle('Misclassifications')
+    num_images = 0
+    
+    for gradcam, in cams:
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            pred_num = pred.cpu().numpy()
+            labels = target.cpu().numpy()
+            index = 0
+            for label, predict in zip(labels, pred_num):
+                if label != predict[0]:
+                    num_images += 1
+                    input_image = data[index]
+                    unnorm_image = UnNormalize(*basic_config.data['normalize_paras'])
+                    unnorm_image_result = unnorm_image(input_image)
+                    mask, _ = gradcam(unnorm_image_result[np.newaxis, :])
+                    heatmap, result = visualize_cam(mask, unnorm_image_result)
+                    #unnorm_image = UnNormalize(*basic_config.data['normalize_paras'])
+                    #unnorm_image_result = unnorm_image(result)
+                    p = plt.subplot((num_of_images/5),5,num_images)
+                    p.imshow(np.transpose(result.cpu(),((1, 2, 0))))
+                    #p.imshow(np.transpose(result,((1, 2, 0))))
                     p.set_xticks(()); p.set_yticks(()) # remove ticks
                     p.set_title(f"Prediction: {classes[predict[0]]},\n Actual: {classes[label]}")
                 index +=1
