4f1d44429735501211c04deb52ee4819f55ba893
Ezhirko <45531125+Ezhirko@users.noreply.github.com>
2020-10-16 21:07:09 +0530

    Update misc_utils.py

--- utils/misc_utils.py
+++ utils/misc_utils.py
@@ -1,7 +1,9 @@
 import torch
 import sys
 from types import ModuleType
-from configs import basic_config 
+from configs import basic_config
+from sklearn.cluster import KMeans
+from matplotlib import pyplot as plt
 
 from utils.logger_utils import get_logger
 
@@ -24,6 +26,26 @@ def current_config():
     for item in dir(basic_config):
       if (not item.startswith("_")) and (not isinstance(getattr(basic_config, item), ModuleType)):
         print(f"{item} - {getattr(basic_config, item)}")
+
+def plot_kmeans_clusters_analysis(data, max_clusters=10):
+  wcss = []
+  for i in range(1, max_clusters+1):
+      kmeans = KMeans(n_clusters=i, init='k-means++', max_iter=300, n_init=10, random_state=0)
+      kmeans.fit(data)
+      wcss.append(kmeans.inertia_)
+  plt.plot(range(1, max_clusters+1), wcss)
+  plt.title('Elbow Method')
+  plt.xlabel('Number of clusters')
+  plt.ylabel('WCSS') # Within cluster sum of squares
+  plt.show()
+  
+def scatter_plot(x_data,y_data,title,x_label,y_label):
+    plt.title(title)
+    plt.scatter(x_data,y_data)
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.show()
+    plt.clf()
 
 if __name__ == "__main__":
     set_manual_seed(20)
